3b4c97747b09c6bf403345237320ef1055cb97c2
Davide Galletti <d.galletti99@gmail.com>
2021-03-05 19:17:04 +0100

    risoluzione errori vari

--- lume/lume/lume.Android/CustomEntryRenderer.cs
+++ lume/lume/lume.Android/CustomEntryRenderer.cs
@@ -3,12 +3,6 @@ using Xamarin.Forms.Platform.Android;
 using Android.Graphics.Drawables;
 using Android.Content;
 using lume.Droid.Renderers;
-using Android.Widget;
-using Android.Runtime;
-using Android.Support.V4.Content;
-using Android.Graphics;
-using Android.OS;
-using Android.Content.Res;
 using System;
 
 [assembly: ExportRenderer(typeof(lume.LumEntry), typeof(CustomEntryRenderer))]
@@ -16,8 +10,7 @@ namespace lume.Droid.Renderers
 {
     class CustomEntryRenderer : EntryRenderer
     {
-        public CustomEntryRenderer(Context context) : base(context)
-        { }
+        public CustomEntryRenderer(Context context) : base(context) { }
 
         protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
         {
@@ -25,24 +18,14 @@ namespace lume.Droid.Renderers
 
             if (Control == null || e.NewElement == null) return;
             
-            if (e.NewElement is LumEntry customEntry)
-            {
-                IntPtr IntPtrtextViewClass = JNIEnv.FindClass(typeof(TextView));
-                IntPtr mCursorDrawableResProperty = JNIEnv.GetFieldID(IntPtrtextViewClass, "mCursorDrawableRes", "I");
-
-                // my_cursor è un file xml presente in drawable
-                JNIEnv.SetField(Control.Handle, mCursorDrawableResProperty, Resource.Drawable.my_cursor);
-
-                borderSet(customEntry);
-                
-            }
+            if (e.NewElement is LumEntry customEntry) borderSet(customEntry);
         }
 
         private void borderSet(LumEntry customEntry)
         {
             var gradientDrawable = new GradientDrawable();
 
-            gradientDrawable.SetCornerRadius(customEntry.CornerRadius*2);
+            gradientDrawable.SetCornerRadius(customEntry.CornerRadius * 2);
             gradientDrawable.SetStroke((int)customEntry.BorderWidth, color: customEntry.BorderColor.ToAndroid());
 
             
