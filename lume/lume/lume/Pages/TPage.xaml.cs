fba1c75e98ca0f9b3d68a4a0a538f5912715ed9e
Davide Galletti <d.galletti99@gmail.com>
2021-03-07 02:22:42 +0100

    collegamento homepage e notification page

--- lume/lume/lume/Pages/TPage.xaml.cs
+++ lume/lume/lume/Pages/TPage.xaml.cs
@@ -20,31 +20,11 @@ namespace lume.Pages
             On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
         }
 
-        int count = 1;
-
-        async void AreUSoure(object sender, EventArgs e)
-        {
-            bool answer = await DisplayAlert("Chiudi", "Seisicuro di voler uscire?", "no", "si");
-            if (answer)
-            {
-                await DisplayAlert("no","","ok");
-            }
-            else
-            {
-                await DisplayAlert("si", "", "ok");
-            }
-        }
+        
 
         protected override bool OnBackButtonPressed()
         {
-            if (count == 0x2)
-            {
-                AreUSoure(this, null);
-                count = 0;
-            }
-            count++;
-            
-            return true;
+            return base.OnBackButtonPressed(); 
         }
     }
 }
