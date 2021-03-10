e326572822b51f41c4e2ef65c8572033d02bb192
Davide Galletti <d.galletti99@gmail.com>
2021-03-10 19:01:23 +0100

    Hyperlink to RegistrationPage

--- lume/lume/lume/Pages/LoginPage.xaml.cs
+++ lume/lume/lume/Pages/LoginPage.xaml.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Reflection;
 using lume.Pages;
 using Xamarin.Forms;
 using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using System.Diagnostics;
 
 namespace lume
 {
     public partial class LoginPage : ContentPage
     {
+        public ICommand TapCommand => new Command(() => Navigation.PushModalAsync(new RegistrationPage(), true));
+
         public LoginPage()
         {
             InitializeComponent();
+
+            BindingContext = this;
         }
+
+
 
         public async void OnClikedButton(object sender, EventArgs e)
         {
