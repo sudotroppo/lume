f9130e35371fe17f0bf8528e308c6cb9691bd4f8
Lorenzo Cannella <lo.cannella7@gmail.com>
2021-05-07 11:58:08 +0200

    varie modifiche

--- API/routes/user.js
+++ API/routes/user.js
@@ -4,40 +4,42 @@ const router = express.Router();
 const db = require("../services/database");
 
 
-	
-	/*const getUserIdByEmail = async (req, res, next) => {
-	req.data = await db.user.findUnique({
-		where: {
-		 email: req.body.email,
-		},
-	 });
-	next();
-	};
 
-	router.get('/', (req, res, next) => {
-		res.json();
-	});
-	*/
+	router.post('/', async(req, res, next) => {
 
-	router.post('/', (req, res, next) => {
-		const user = {
+		const user = await db.user.create({
+		data:{
 			email: req.body.email,
 			nome: req.body.nome,
 			cognome: req.body.cognome
-		};
+			}
+		});
 
-		db.user.create({ data: user })
-                    .catch((err) => {
-                        return res.status(500).json({
-                            error: err
-                        });
-
-                    });
+        console.log('Created new user: ', user)  
 		res.status(201).json({
 			message: "Post di user",
-			Utente:user
+			user:user
 		});
 
 	});
 
+	router.get('/:requestId', async(req, res,next) => {
+		const user = await db.user.findUnique({
+		where: {
+			id: Number(req.params.requestId),
+		},
+		select: {
+			nome:true,
+			cognome:true,
+			email:true,
+		}
+		})
+
+	
+		res.status(201).json({
+			message: "Get di user by Id",
+			user:user
+		});
+	});
+	
 module.exports = router;
